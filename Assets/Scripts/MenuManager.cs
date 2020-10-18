efd21b7bc97969b6afecdc2ecb956f8e1f8e8e7c
Marcelo Nogueira <66036697+lethalarms23@users.noreply.github.com>
2020-10-18 22:21:25 +0100

    Menu Básico

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject MenuPT;
+    public GameObject Jogo;
+
+    public void EnterGame(){
+        MenuPT.SetActive(false);
+        Jogo.SetActive(true);
+    }
+
+    public void FixedUpdate() {
+        if(Input.GetKeyUp(KeyCode.Escape)){
+            MenuPT.SetActive(true);
+            Jogo.SetActive(false);
+        }
+    }
+}
