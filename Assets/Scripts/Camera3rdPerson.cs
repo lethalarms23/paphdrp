632e2ab4684f8f4e45f082abfe90450383a9b7a1
Marcelo Nogueira <66036697+lethalarms23@users.noreply.github.com>
2020-12-13 00:52:32 +0000

    Adiciona novo carro + Árvores
    Adicionada Nissan Titan Warrior Concept (Modelo, Condução,Camera,etc)
    Mapa Principal Re-importado/Extraidos Materias para ativar alpha-clipping dos materias das árvores

--- Assets/Scripts/Camera3rdPerson.cs
+++ Assets/Scripts/Camera3rdPerson.cs
@@ -8,12 +8,15 @@ public class Camera3rdPerson : MonoBehaviour
     public float mouseSensivity;
     public Transform target;
     public float distanceFromTarget = 2;
+    
     public Vector2 pitchMinMax = new Vector2(-40, 85);
 
     public float rotationSmoothTime = .12f;
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
 
+    float zoomSens=2;
+    float zoom;
     float yaw;
     float pitch;
 
@@ -27,9 +30,15 @@ public class Camera3rdPerson : MonoBehaviour
         }
     }
 
+    public void zoomTarget(){
+        zoom = -(Input.GetAxis("Mouse ScrollWheel") * zoomSens);
+        distanceFromTarget += zoom;
+    }
+
     //Atribui as variaveis o input do rato e consoante o valor gira em volta do target(personagem)
     void LateUpdate()
     {
+        zoomTarget();
         yaw += Input.GetAxis("Mouse X") * mouseSensivity;
         pitch -= Input.GetAxis("Mouse Y") * mouseSensivity;
         pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
---
+++ Assets/Scripts/RigidCenterOfMass.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidCenterOfMass : MonoBehaviour
+{
+    public Vector3 com;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = com;
+    }
+}
