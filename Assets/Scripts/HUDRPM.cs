175271cc2c8281ea498cace9722844d5113ff276
Marcelo Nogueira <66036697+lethalarms23@users.noreply.github.com>
2021-02-20 21:33:39 +0000

    Otimização das Texturas

---
+++ Assets/Scripts/HUDRPM.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDRPM : MonoBehaviour
+{
+    public WheelCollider rearDriverW;
+    public Transform ponteiro;
+    public float z = 0;
+
+    public void Start() {
+        z = ponteiro.eulerAngles.z;
+    }
+
+    public void FixedUpdate() {
+        if(rearDriverW.rpm < 1){
+            z =  80 + (rearDriverW.rpm / 10);
+        }
+        else if(rearDriverW.rpm > -1){
+            z =  80 - (rearDriverW.rpm / 10);
+        }
+    	Vector3 steer = new Vector3(0,0,z);
+        ponteiro.eulerAngles = steer;
+    }   
+}
